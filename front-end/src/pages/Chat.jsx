ef2a3b3e3a9627b6383f2d299230aef9676cd25f
Junaid khan <jk387520@gmail.com>
2024-11-26 16:45:20 +0530

    Handling input field

--- front-end/src/pages/Chat.jsx
+++ front-end/src/pages/Chat.jsx
@@ -43,10 +43,10 @@ export default function Chat() {
             <ChatContainer socket={socket.current} />
           </>
         ) : (
-          <>
-            <Contacts socket={socket.current} currentUser={curUser} />
-            <ChatContainer socket={socket.current} />
-          </>
+          <div className="loader">
+            <img src={loader} alt="loading" className="loader-img" />
+            <h2>Loading Environment</h2>
+          </div>
         )}
       </div>
     </Container>
