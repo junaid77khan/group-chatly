73d1bb8a7434f6b75fd16016b5802c06c2ec807e
Junaid Khan <junaidk8185@gmail.com>
2024-12-10 00:56:14 +0530

    Setting aws server

--- front-end/src/utils/APIRoutes.js
+++ front-end/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://15.207.43.129:5000";
+export const host = "https://group-chatly-server.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
